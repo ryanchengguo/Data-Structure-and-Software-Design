cf776c5799862f69de434547cad05377750c4656
Ryan Guo <guohao_lin@msn.com>
2019-03-06 20:52:07 -0800

    Update addToHeap.java

--- Week2.7 Adding to a Heap/addToHeap.java
+++ Week2.7 Adding to a Heap/addToHeap.java
@@ -1,14 +1,6 @@
 
 public class addToHeap {
 
-	public void add(int value){
-		if(size == values.length){
-			throw new HeapException();
-		}
-		values[size] = value;
-		bubbleUp(size);
-		size++;
-	}
 
 	public void bubbleUp(int index){
 
